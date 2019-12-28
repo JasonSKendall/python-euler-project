94add16b02c8a6af0d8bb73c21e223b8b4153cd1
Jason Kendall <59287362+JasonSKendall@users.noreply.github.com>
2019-12-28 01:07:26 -0500

    Update euler001.py

--- euler001.py
+++ euler001.py
@@ -1,47 +1,46 @@
 #!python
 
-problem =3D """
-The sum of the primes below 10 is 2 + 3 + 5 + 7 =3D 17.
+problem = """
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes below two million.
 
-https://projecteuler.net/problem=3D10
+https://projecteuler.net/problem=10
 
 """
 
 print(problem)
 
-top =3D 2000000
-primeset =3D [2,3]
+top = 2000000
+primeset = [2,3]
 
-possibles=3D[]
+possibles=[]
 
 
-x=3D3
+x=3
 while x < top:
   possibles.append(x)
-  x+=3D2
+  x+=2
 
-p=3D3
+p=3
 while (len(possibles)) > 0:
-  zoop1=3Dlen(possibles)
-  zoop2=3Dlen(primeset)
+  zoop1=len(possibles)
+  zoop2=len(primeset)
   print("current prime:",p,",possibles:",zoop1,",  primeset:",zoop2)
   for x in possibles:
-    if ( x % p ) =3D=3D 0:
+    if ( x % p ) == 0:
       possibles.remove(x)
-  p=3Dpossibles[0]
+  p=possibles[0]
   primeset.append(p)
   del possibles[0]
   if p > top**(1/2):
     break
 
-allprimes=3Dprimeset+possibles
+allprimes=primeset+possibles
 
-sum=3D0
-n=3D1
+sum=0
+n=1
 for p in allprimes:
-  sum+=3Dp
+  sum+=p
   print(n,p,sum)
-  n+=3D1
-
+  n+=1
